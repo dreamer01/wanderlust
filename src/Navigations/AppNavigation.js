be600fb8c3255cb90585fa5a50f6954b1fe926e3
Kuliza-280 <psnitsri01@gmail.com>
2020-10-23 13:32:27 +0530

    Navigating to Welcome view Directly

--- src/Navigations/AppNavigation.js
+++ src/Navigations/AppNavigation.js
@@ -1,17 +1,15 @@
 import {createAppContainer, createSwitchNavigator} from 'react-navigation';
 import Routes from './Routes';
-import {AuthNavigator} from './AuthNavigator';
-import AuthLoadingView from '../Views/GetStarted/AuthLoadingView';
+import WelcomeView from '../Views/GetStarted/WelcomeView';
 import {TabNavigator} from './TabNavigator';
 
 const AppNavigator = createSwitchNavigator(
   {
-    [Routes.AuthLoadingView]: AuthLoadingView,
     [Routes.TabNavigator]: TabNavigator,
-    [Routes.AuthNavigator]: AuthNavigator,
+    [Routes.WelcomeView]: WelcomeView,
   },
   {
-    initialRouteName: Routes.AuthLoadingView,
+    initialRouteName: Routes.WelcomeView,
   },
 );
 
--- src/Navigations/AuthNavigator.js
+++ src/Navigations/AuthNavigator.js
@@ -1,21 +1,22 @@
-import { createStackNavigator } from 'react-navigation'
-import WelcomeView from '../Views/GetStarted/WelcomeView'
-import LoginView from '../Views/GetStarted/LoginView'
-import SignUpView from '../Views/GetStarted/SignUpView'
-import Routes from './Routes'
-import ForgotPasswordView from '../Views/GetStarted/ForgotPasswordView'
+import {createStackNavigator} from 'react-navigation';
+import WelcomeView from '../Views/GetStarted/WelcomeView';
+import LoginView from '../Views/GetStarted/LoginView';
+import SignUpView from '../Views/GetStarted/SignUpView';
+import Routes from './Routes';
+import ForgotPasswordView from '../Views/GetStarted/ForgotPasswordView';
 
 export const AuthNavigator = createStackNavigator(
   {
-    [Routes.WelcomeView]: { screen: WelcomeView },
-    [Routes.LoginView]: { screen: LoginView },
-    [Routes.SignUpView]: { screen: SignUpView },
-    [Routes.ForgotPasswordView]: { screen: ForgotPasswordView }
+    [Routes.WelcomeView]: {screen: WelcomeView},
+    [Routes.LoginView]: {screen: LoginView},
+    [Routes.SignUpView]: {screen: SignUpView},
+    [Routes.ForgotPasswordView]: {screen: ForgotPasswordView},
   },
   {
-    navigationOptions: ({ navigation }) => ({
-      gesturesEnabled: false
+    navigationOptions: ({navigation}) => ({
+      gesturesEnabled: false,
     }),
-    initialRouteName: Routes.WelcomeView
-  }
-)
+    headerMode: 'none',
+    initialRouteName: Routes.WelcomeView,
+  },
+);
