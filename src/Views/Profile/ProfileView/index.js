8970de89977ea22a2308b6c8b4696311abc3ce6b
Kuliza-280 <psnitsri01@gmail.com>
2020-10-26 16:05:14 +0530

    Showing logged user profile and adding queries

--- src/Views/Profile/ProfileView/index.js
+++ src/Views/Profile/ProfileView/index.js
@@ -1,13 +1,12 @@
-import React, {Component} from 'react';
+import React, {Component, useEffect, useState} from 'react';
 import {SafeAreaView, View, Image, Text, TextInput} from 'react-native';
 import {NavigationActions} from 'react-navigation';
 import AsyncStorage from '@react-native-community/async-storage';
 
 import Routes from '../../../Navigations/Routes';
 import I18n from '../../../localization/i18n';
-import auth0 from '../../../Config/auth0';
-import {Icons} from '../../../Constants/Assets';
-import {Places, Place} from '../../../Constants/Constants';
+import {useUser} from '../../../Utils/userContext';
+import {Places} from '../../../Constants/Constants';
 import AppButton from '../../../Components/base-componets/AppButton';
 import TitleNavigationHeader from '../../../Components/navigation-header/TitleNavigationHeader';
 import ManageKeyboardScrollView from '../../../Constants/ManageKeyboardScrollView';
@@ -15,161 +14,116 @@ import PlaceCollectionView from '../../../Components/PlaceCollectionView';
 
 import styles from './styles';
 
-class ProfileView extends Component {
-  static navigationOptions = {
-    header: null,
-  };
+const ProfileView = ({navigation}) => {
+  const [user, userDispatch] = useUser();
 
-  state = {
-    name: 'Maahi Bhama',
-    email: 'maahibhama@gmail.com',
-    password: 'way2sucess',
-  };
-
-  onClickRightButton = this.onClickRightButton.bind(this);
-  logoutButtonAction = this.logoutButtonAction.bind(this);
-
-  onClickRightButton() {}
-
-  logoutButtonAction() {
+  const logoutButtonAction = () => {
     // TODO: Uncomment currently commented to avoid popups
     // auth0.webAuth.clearSession({}).catch(error => console.log(error));
     AsyncStorage.clear()
       .then(() =>
-        this.props.navigation.dispatch(
-          NavigationActions.navigate({routeName: Routes.AuthNavigator}),
+        navigation.dispatch(
+          NavigationActions.navigate({routeName: Routes.WelcomeView}),
         ),
       )
       .catch(error => console.log(error));
-  }
+  };
 
-  render() {
-    return (
-      <SafeAreaView style={styles.container}>
-        <TitleNavigationHeader
-          navigation={this.props.navigation}
-          title={I18n.t('profile01')}
-          showRightButton
-          rightButtonIcon={Icons.profileEdit}
-          onClickRightButton={this.onClickRightButton}
-        />
-        <ManageKeyboardScrollView
-          keyboardShouldPersistTaps={'always'}
-          contentContainerStyle={styles.keyboardAvoidView}>
-          {this.renderMiddleView()}
-        </ManageKeyboardScrollView>
-      </SafeAreaView>
-    );
-  }
-
-  renderMiddleView() {
+  const renderMiddleView = () => {
     return (
       <View style={styles.middleView}>
-        {this.renderProfileInfoView()}
-        {this.renderNameInput()}
-        {this.renderEmailAddress()}
-        {this.renderPasswordInput()}
-        {this.renderVisitedPlaces()}
-        {this.renderLogoutButton()}
+        {renderProfileInfoView()}
+        {renderVisitedPlaces()}
+        {renderVisitedPlaces()}
+        {renderLogoutButton()}
       </View>
     );
-  }
+  };
 
-  renderProfileInfoView() {
+  const renderProfileInfoView = () => {
     return (
       <View style={styles.profileView}>
         <View style={styles.imageContainer}>
           <Image
-            source={{uri: Place.url}}
+            source={{uri: user && user.picture}}
             resizeMode={'cover'}
             resizeMethod={'resize'}
             style={styles.profileImage}
           />
         </View>
         <View style={styles.titleDetailsView}>
-          <Text style={styles.titleText}>{'Maahi Bhama'}</Text>
-          <Text style={styles.subtitleText}>{'A developer'}</Text>
+          <Text style={styles.titleText}>{user && user.name}</Text>
+          <Text style={styles.subtitleText}>{user && user.email}</Text>
         </View>
       </View>
     );
-  }
+  };
 
-  renderNameInput() {
-    return (
-      <TextInput
-        underlineColorAndroid={'transparent'}
-        ref={'nameTextField'}
-        placeholder={I18n.t('signUp02')}
-        autoCapitalize={'sentences'}
-        returnKeyType={'next'}
-        autoCorrect={false}
-        style={styles.inputViewStyle}
-        onChangeText={text => this.setState({email: text})}
-        onSubmitEditing={event => {
-          this.refs.emailTextField.focus();
-        }}
-        value={this.state.name}
-      />
-    );
-  }
+  // const renderNameInput = () => {
+  //   return (
+  //     <TextInput
+  //       underlineColorAndroid={'transparent'}
+  //       placeholder={I18n.t('signUp02')}
+  //       autoCapitalize={'sentences'}
+  //       returnKeyType={'next'}
+  //       autoCorrect={false}
+  //       style={styles.inputViewStyle}
+  //       onChangeText={text => {}}
+  //       value={user && user.name}
+  //     />
+  //   );
+  // };
 
-  renderEmailAddress() {
-    return (
-      <TextInput
-        underlineColorAndroid={'transparent'}
-        ref={'emailTextField'}
-        placeholder={I18n.t('signIn02')}
-        keyboardType={'email-address'}
-        autoCapitalize={'none'}
-        returnKeyType={'next'}
-        autoCorrect={false}
-        style={styles.inputViewStyle}
-        onChangeText={text => this.setState({email: text})}
-        onSubmitEditing={event => {
-          this.refs.passwordTextField.focus();
-        }}
-        value={this.state.email}
-      />
-    );
-  }
+  // const renderEmailAddress = () => {
+  //   return (
+  //     <TextInput
+  //       underlineColorAndroid={'transparent'}
+  //       placeholder={I18n.t('signIn02')}
+  //       keyboardType={'email-address'}
+  //       autoCapitalize={'none'}
+  //       returnKeyType={'next'}
+  //       autoCorrect={false}
+  //       style={styles.inputViewStyle}
+  //       onChangeText={text => {}}
+  //       value={user && user.email}
+  //     />
+  //   );
+  // };
 
-  renderPasswordInput() {
-    return (
-      <TextInput
-        underlineColorAndroid={'transparent'}
-        ref={'passwordTextField'}
-        placeholder={I18n.t('signIn03')}
-        returnKeyType={'done'}
-        autoCapitalize={'none'}
-        autoCorrect={false}
-        secureTextEntry={!this.state.isShowingPassword}
-        style={styles.inputViewStyle}
-        onChangeText={text => this.setState({password: text})}
-        onSubmitEditing={this.signInButtonAction}
-        value={this.state.password}
-      />
-    );
-  }
-
-  renderVisitedPlaces() {
+  const renderVisitedPlaces = () => {
     return (
       <PlaceCollectionView
-        navigation={this.props.navigation}
+        navigation={navigation}
         data={Places}
         headerTitle={I18n.t('profile02')}
       />
     );
-  }
+  };
 
-  renderLogoutButton() {
+  const renderLogoutButton = () => {
     return (
       <AppButton
         title={I18n.t('profile03')}
-        onTouch={this.logoutButtonAction}
+        onTouch={logoutButtonAction}
         styles={styles.logoutButtonStyle}
         textStyles={styles.logoutButtonTextStyle}
       />
     );
-  }
-}
+  };
+
+  return (
+    <SafeAreaView style={styles.container}>
+      <TitleNavigationHeader
+        navigation={navigation}
+        title={I18n.t('profile01')}
+        showRightButton
+      />
+      <ManageKeyboardScrollView
+        keyboardShouldPersistTaps={'always'}
+        contentContainerStyle={styles.keyboardAvoidView}>
+        {renderMiddleView()}
+      </ManageKeyboardScrollView>
+    </SafeAreaView>
+  );
+};
 export default ProfileView;
